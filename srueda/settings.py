d99a815ced5e2478862125e10c0ce7a733be3068
sergio rueda <sergiorueda90@hotmail.com>
2026-03-21 14:56:52 -0500

    cambio de db

--- srueda/settings.py
+++ srueda/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'srueda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'srueda',
-        'USER': 'root',
-        'PASSWORD': '***Sergio3143801560---',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
